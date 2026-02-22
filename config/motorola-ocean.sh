e7a4d737cb5f6071c5569162c913deaca1daeed9
Val Packett <val@packett.cool>
2026-02-21 21:33:14 -0300

    config: motorola-ocean: rename panels to match lk2nd

--- config/motorola-ocean.sh
+++ config/motorola-ocean.sh
@@ -3,7 +3,7 @@
 
 OPTIONS=(-r vsn -r vsp)
 PANELS=(
-	[tianma_622_720p_vid_v0]="tianma,622-v0"
-	[ofilm_622_720p_vid_v0]="ofilm,622-v0"
-	[csot_622_720p_vid_v0]="csot,622-v0"
+	[tianma_622_720p_vid_v0]="motorola,ocean-622-tianma"
+	[ofilm_622_720p_vid_v0]="motorola,ocean-622-ofilm"
+	[csot_622_720p_vid_v0]="motorola,ocean-nt36672a-csot"
 )
