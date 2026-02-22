368d7de78f17712715245c938b7884d2a817b99c
Val Packett <val@packett.cool>
2026-02-21 21:43:45 -0300

    config/dtb: Add Moto G7 (river)

---
+++ config/motorola-river.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+OPTIONS=(-r vsn -r vsp)
+PANELS=(
+	[boe_624_1080p_vid_v0]="motorola,river-624-boe"
+	[tianma_624_1080p_vid_v0]="motorola,river-624-tianma"
+	[tianma_n_624_1080p_vid_v0]="motorola,river-nt36672a-tianma"
+	[tianma_h_624_1080p_vid_v0]="motorola,river-hx83112b-tianma"
+)
