30188dfeb4fd1fd45da7e1b197873d4803715d97
BHlmaoMSD <memay761@gmail.com>
2026-04-22 12:03:58 +0700

    config/dtb: Add Vsmart Joy 3 (casuarina)

---
+++ config/vsmart-casuarina.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+OPTIONS=(-r vsn -r vsp)
+PANELS=(
+	[ft8006p_hlt_1600_video]="vsmart,casuarina-ft8006p-hlt"
+	[ft8006p_truly_1600_video]="vsmart,casuarina-ft8006p-truly"
+	[icnl9911s_byd_1600_video]="vsmart,casuarina-icnl9911s-byd"
+)
