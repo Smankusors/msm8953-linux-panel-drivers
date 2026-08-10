68d7858460aa1142b523a37c6d3c10a1d72c6d8e
Antony Kurniawan Soemardi <linux@smankusors.com>
2026-08-10 22:44:29 +0700

    config/asus-zenfone3: rename asus zenfone 3 panels to codenames

--- config/asus-zenfone3.sh
+++ config/asus-zenfone3.sh
@@ -2,10 +2,10 @@
 
 OPTIONS=(-r vsn -r vsp)
 PANELS=(
-	[tm5p2_r63350_1080p_video]="asus,ze520kl-r63350-tm"
-	[boe5p2_ili7807b_1080p_video]="asus,ze520kl-ili7807b-boe"
-	[ctc5p5_ili7807b_1080p_video]="asus,ze552kl-ili7807b-ctc"
-	[tm5p5_r63350_1080p_video]="asus,ze552kl-r63350-tm"
-	[txd5p5_nt35596_1080p_video]="asus,ze552kl-nt35596-txd"
-	[lce5p5_otm1901a_1080p_video]="asus,ze552kl-otm1901a-lce"
+	[tm5p2_r63350_1080p_video]="asus,leo-r63350-tm"
+	[boe5p2_ili7807b_1080p_video]="asus,leo-ili7807b-boe"
+	[ctc5p5_ili7807b_1080p_video]="asus,libra-ili7807b-ctc"
+	[tm5p5_r63350_1080p_video]="asus,libra-r63350-tm"
+	[txd5p5_nt35596_1080p_video]="asus,libra-nt35596-txd"
+	[lce5p5_otm1901a_1080p_video]="asus,libra-otm1901a-lce"
 )
